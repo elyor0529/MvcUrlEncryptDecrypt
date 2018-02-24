e8a4910aee480a029bc85170093a927ccd29a834
Elyor Latipov <elyor.blog@gmail.com>
2018-02-24 16:53:46 +0500

    clean

---
+++ MvcUrlEncryptDecrypt/MvcUrlEncryptDecrypt.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using MvcUrlEncryptDecrypt.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUrlEncryptDecrypt.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void E(string id)
+        {
+            var result = EncryptDecryptHelper.EncryptString(id);
+
+            Response.Write(result);
+        }
+
+        public void D(string id)
+        {
+            var result = EncryptDecryptHelper.DecryptString(id);
+
+            Response.Write(result);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcUrlEncryptDecrypt/MvcUrlEncryptDecrypt.Web/Helpers/EncryptDecryptHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace MvcUrlEncryptDecrypt.Web.Helpers
+{
+    public class EncryptDecryptHelper
+    {
+        private const string KEY = "KMNOPQASWED";
+
+        public static string EncryptString(string s)
+        {
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(s);
+            var hashmd5 = new MD5CryptoServiceProvider();
+            var keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(KEY));
+            hashmd5.Clear();
+
+            var tdes = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            var cTransform = tdes.CreateEncryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            tdes.Clear();
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public static string DecryptString(string s)
+        {
+            byte[] toEncryptArray = Convert.FromBase64String(s);
+            var hashmd5 = new MD5CryptoServiceProvider();
+            var keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(KEY));
+            hashmd5.Clear();
+
+            var tdes = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            var cTransform = tdes.CreateDecryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            tdes.Clear();
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+    }
+}
